6b2cf93b851b76956f491410304381917e0a81e7
Gabriel Prates <gabsprates@gmail.com>
2018-07-12 17:16:33 -0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,46 @@
-var path = require('path');
-module.exports = {
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-  entry: './src/js/app.js',
+module.exports = {
+  entry: ["./src/app.js", "./src/sass/index.scss"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: path.resolve(__dirname, 'node_modules'),
-        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: "babel-loader",
         options: {
           presets: ["es2015", "react"]
         }
       },
       {
         test: /\.scss$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: "[sha1:hash:7]"
+              },
+            },
+            "postcss-loader",
+            "sass-loader"
+          ]
+        })
       }
     ]
   },
+
+  plugins: [ new ExtractTextPlugin("style.css") ],
 
   devServer: {
     inline: true
